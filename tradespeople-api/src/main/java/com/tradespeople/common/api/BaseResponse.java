e96dd6dd2a2d51fc90d39a10ee2b9e5e1891076f
cetinimre <cetin_imre@gmail.com>
2013-08-25 17:16:13 +0300

    BaseStatus and ShopCategoryStatus added

--- tradespeople-api/src/main/java/com/tradespeople/common/api/BaseResponse.java
+++ tradespeople-api/src/main/java/com/tradespeople/common/api/BaseResponse.java
@@ -4,13 +4,13 @@ import com.tradespeople.utils.ApiConstants;
 
 public class BaseResponse {
 	
-	private ApiConstants.ResponseErrorCode errorCode;
+	private ApiConstants.Enums.ResponseErrorCode errorCode;
 	private String errorMessage;
-	private ApiConstants.ResponseStatus status;
+	private ApiConstants.Enums.ResponseStatus status;
 	
 	public BaseResponse(){
-		this.errorCode=ApiConstants.ResponseErrorCode.NoError;
-		this.setStatus(ApiConstants.ResponseStatus.SuccessFull);
+		this.errorCode=ApiConstants.Enums.ResponseErrorCode.NoError;
+		this.setStatus(ApiConstants.Enums.ResponseStatus.SuccessFull);
 		this.errorMessage="";
 	}
 	public String getErrorMessage() {
@@ -19,10 +19,10 @@ public class BaseResponse {
 	public void setErrorMessage(String errorMessage) {
 		this.errorMessage = errorMessage;
 	}
-	public ApiConstants.ResponseErrorCode getErrorCode() {
+	public ApiConstants.Enums.ResponseErrorCode getErrorCode() {
 		return errorCode;
 	}
-	public void setErrorCode(ApiConstants.ResponseErrorCode errorCode) {
+	public void setErrorCode(ApiConstants.Enums.ResponseErrorCode errorCode) {
 		this.errorCode = errorCode;
 	}
 	
@@ -32,13 +32,13 @@ public class BaseResponse {
 	}
 	public static BaseResponse fail() {
 		BaseResponse failBase=new BaseResponse();
-		failBase.setStatus(ApiConstants.ResponseStatus.Fail);
+		failBase.setStatus(ApiConstants.Enums.ResponseStatus.Fail);
 		return failBase;
 	}
-	public ApiConstants.ResponseStatus getStatus() {
+	public ApiConstants.Enums.ResponseStatus getStatus() {
 		return status;
 	}
-	public void setStatus(ApiConstants.ResponseStatus status) {
+	public void setStatus(ApiConstants.Enums.ResponseStatus status) {
 		this.status = status;
 	}
 
--- tradespeople-api/src/main/java/com/tradespeople/utils/ApiConstants.java
+++ tradespeople-api/src/main/java/com/tradespeople/utils/ApiConstants.java
@@ -2,16 +2,29 @@ package com.tradespeople.utils;
 
 public class ApiConstants {
 
-	public static enum ResponseErrorCode{
-		NoError("0"),NotSupportedParameter("1");
-		ResponseErrorCode(String errorCode){
+	public static class Enums {
+		
+		public static enum BaseStatus{
+			Active("0"), Passive("1");
+			BaseStatus(String statusCode) {
+			}
+		}
+		public static enum ResponseErrorCode {
+			NoError("0"), NotSupportedParameter("1");
+			ResponseErrorCode(String errorCode) {
+			}
+		}
+
+		public static enum ShopCategoryStatus {
+			Active("0"), Passive("1");
+			ShopCategoryStatus(String statusCode) {
+			}
+		}
+
+		public static enum ResponseStatus {
+			SuccessFull("0"), Fail("1");
+			ResponseStatus(String errorCode) {
+			}
 		}
 	}
-	
-	public static enum ResponseStatus{
-		SuccessFull("0"),Fail("1");
-		ResponseStatus(String errorCode){
-		}
-	}
-	
 }
