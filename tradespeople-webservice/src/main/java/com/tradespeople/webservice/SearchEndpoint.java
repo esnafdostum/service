b8b1d0a6f506ebd797c3fbf0c0dab87826aa7680
cetinimre <cetin_imre@gmail.com>
2013-09-15 18:49:35 +0300

    Search End Point

---
+++ tradespeople-webservice/src/main/java/com/tradespeople/webservice/SearchEndpoint.java
@@ -0,0 +1,12 @@
+package com.tradespeople.webservice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.tradespeople.common.api.BaseController;
+
+@Controller
+@RequestMapping("/search")
+public class SearchEndpoint extends BaseController {
+
+}
